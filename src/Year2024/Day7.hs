f35a746251cb441ed43bf4e758c0d09e1b266c2a
pdcxs <pdcxs@outlook.com>
2025-01-02 00:40:22 +0800

    2024 day 07

---
+++ src/Year2024/Day7.hs
@@ -0,0 +1,41 @@
+module Year2024.Day7 (solution1, solution2) where
+
+processInput :: String -> [(Int, [Int])]
+processInput = map processLine . lines
+  where
+    processLine l = (target l, remains l)
+    target = read . takeWhile (/= ':')
+    remains = map read . words . tail . dropWhile (/= ':')
+
+type Op = Int -> Int -> Int
+
+combs :: [Op] -> [Int] -> [Int]
+combs _ [] = []
+combs _ [x] = [x]
+combs ops (x : y : xs) =
+  concatMap
+    (\n -> combs ops (n : xs))
+    nums
+  where
+    nums = map (\f -> f x y) ops
+
+isRight :: [Op] -> Int -> [Int] -> Bool
+isRight ops target xs = target `elem` combs ops xs
+
+solution1 :: String -> String
+solution1 =
+  show
+    . sum
+    . map fst
+    . filter (uncurry (isRight [(+), (*)]))
+    . processInput
+
+solution2 :: String -> String
+solution2 =
+  show
+    . sum
+    . map fst
+    . filter (uncurry (isRight [(+), (*), conc]))
+    . processInput
+  where
+    conc x y = read (show x ++ show y)
