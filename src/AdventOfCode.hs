15aee3ed33950b0e5d68d4b85d04ddd04f58625e
pdcxs <pdcxs@outlook.com>
2025-01-18 21:08:36 +0800

    2024 Day 13

--- src/AdventOfCode.hs
+++ src/AdventOfCode.hs
@@ -5,6 +5,7 @@ import qualified Year2024.Day1
 import qualified Year2024.Day10
 import qualified Year2024.Day11
 import qualified Year2024.Day12
+import qualified Year2024.Day13
 import qualified Year2024.Day2
 import qualified Year2024.Day3
 import qualified Year2024.Day4
@@ -17,28 +18,30 @@ import qualified Year2024.Day9
 solutions :: M.Map (Int, Int, Int) (String -> String)
 solutions =
   M.fromList
-    [ ((2024, 1, 1), Year2024.Day1.solution1),
-      ((2024, 1, 2), Year2024.Day1.solution2),
-      ((2024, 2, 1), Year2024.Day2.solution1),
-      ((2024, 2, 2), Year2024.Day2.solution2),
-      ((2024, 3, 1), Year2024.Day3.solution1),
-      ((2024, 3, 2), Year2024.Day3.solution2),
-      ((2024, 4, 1), Year2024.Day4.solution1),
-      ((2024, 4, 2), Year2024.Day4.solution2),
-      ((2024, 5, 1), Year2024.Day5.solution1),
-      ((2024, 5, 2), Year2024.Day5.solution2),
-      ((2024, 6, 1), Year2024.Day6.solution1),
-      ((2024, 6, 2), Year2024.Day6.solution2),
-      ((2024, 7, 1), Year2024.Day7.solution1),
-      ((2024, 7, 2), Year2024.Day7.solution2),
-      ((2024, 8, 1), Year2024.Day8.solution1),
-      ((2024, 8, 2), Year2024.Day8.solution2),
-      ((2024, 9, 1), Year2024.Day9.solution1),
-      ((2024, 9, 2), Year2024.Day9.solution2),
-      ((2024, 10, 1), Year2024.Day10.solution1),
-      ((2024, 10, 2), Year2024.Day10.solution2),
-      ((2024, 11, 1), Year2024.Day11.solution1),
-      ((2024, 11, 2), Year2024.Day11.solution2),
-      ((2024, 12, 1), Year2024.Day12.solution1),
-      ((2024, 12, 2), Year2024.Day12.solution2)
+    [ ((2024, 1, 1), Year2024.Day1.solution1)
+    , ((2024, 1, 2), Year2024.Day1.solution2)
+    , ((2024, 2, 1), Year2024.Day2.solution1)
+    , ((2024, 2, 2), Year2024.Day2.solution2)
+    , ((2024, 3, 1), Year2024.Day3.solution1)
+    , ((2024, 3, 2), Year2024.Day3.solution2)
+    , ((2024, 4, 1), Year2024.Day4.solution1)
+    , ((2024, 4, 2), Year2024.Day4.solution2)
+    , ((2024, 5, 1), Year2024.Day5.solution1)
+    , ((2024, 5, 2), Year2024.Day5.solution2)
+    , ((2024, 6, 1), Year2024.Day6.solution1)
+    , ((2024, 6, 2), Year2024.Day6.solution2)
+    , ((2024, 7, 1), Year2024.Day7.solution1)
+    , ((2024, 7, 2), Year2024.Day7.solution2)
+    , ((2024, 8, 1), Year2024.Day8.solution1)
+    , ((2024, 8, 2), Year2024.Day8.solution2)
+    , ((2024, 9, 1), Year2024.Day9.solution1)
+    , ((2024, 9, 2), Year2024.Day9.solution2)
+    , ((2024, 10, 1), Year2024.Day10.solution1)
+    , ((2024, 10, 2), Year2024.Day10.solution2)
+    , ((2024, 11, 1), Year2024.Day11.solution1)
+    , ((2024, 11, 2), Year2024.Day11.solution2)
+    , ((2024, 12, 1), Year2024.Day12.solution1)
+    , ((2024, 12, 2), Year2024.Day12.solution2)
+    , ((2024, 13, 1), Year2024.Day13.solution1)
+    , ((2024, 13, 2), Year2024.Day13.solution2)
     ]
---
+++ src/Year2024/Day13.hs
@@ -0,0 +1,51 @@
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+
+module Year2024.Day13 (solution1, solution2) where
+
+import Data.Char (isDigit, isSpace)
+import Data.List.Split (splitWhen)
+
+processInput :: String -> [[Integer]]
+processInput s = map (concatMap extraceNumber) inputs
+ where
+  inputs = splitWhen null $ lines s
+  extraceNumber =
+    map read
+      . words
+      . filter (\c -> isDigit c || isSpace c)
+
+isInteger :: Double -> Bool
+isInteger x = x == fromIntegral (round x :: Integer)
+
+-- Just solve an equation
+solve :: [Integer] -> Integer
+solve nums =
+  if isInteger a && isInteger b
+    then
+      3 * round a + round b
+    else 0
+ where
+  [ax, ay, bx, by, tx, ty] = map fromIntegral nums
+  den = ax * by - ay * bx
+  n1 = by * tx - bx * ty
+  n2 = ax * ty - ay * tx
+  a = n1 / den
+  b = n2 / den
+
+solution1 :: String -> String
+solution1 =
+  show
+    . sum
+    . map solve
+    . processInput
+
+solution2 :: String -> String
+solution2 =
+  show
+    . sum
+    . map (solve . fix)
+    . processInput
+ where
+  fix x =
+    take 4 x
+      ++ map (+ 10000000000000) (drop 4 x)
