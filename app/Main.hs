a625d8ebb157863dddf07aa3abc7d1dc3454a557
pdcxs <pdcxs@outlook.com>
2025-03-29 19:39:17 +0800

    Implement text drawing

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,10 @@ module Main (main) where
 import AdventOfCode (animations, solutions)
 import qualified Data.Map as M
 import Data.Maybe (fromMaybe)
+import Linear (V2 (V2))
+import Raylib.Core
+import Raylib.Core.Text
+import Raylib.Util.Colors (black, white)
 import System.Environment (getArgs)
 import System.FilePath ((</>))
 import Text.Read (readMaybe)
@@ -23,10 +27,10 @@ main = do
           let func = solutions M.! (year, day, question)
            in putStrLn $ func contents
         else do
-          let _func = animations M.! (year, day, question)
-              _anims = _func contents -- generate animation texts
-              _delay = getDelay args
-           in putStrLn "not implement yet"
+          let func = animations M.! (year, day, question)
+              anims = func contents -- generate animation texts
+              fps = getArgFPS args
+           in animation anims fps
 
 getTimeQuestion :: [String] -> (Int, Int, Int)
 getTimeQuestion args = (year, day, question)
@@ -42,12 +46,12 @@ getFile (year, day, test) = "inputs" </> show year </> file
   file = prefix ++ formatNum day ++ ".txt"
   prefix = if test then "test" else "input"
 
-getDelay :: [String] -> Double
-getDelay args =
+getArgFPS :: [String] -> Int
+getArgFPS args =
   if length args > 4 && args !! 3 == "test"
     || length args > 3 && args !! 3 /= "test"
-    then fromMaybe 1.0 (readMaybe (last args))
-    else 1.0 :: Double
+    then fromMaybe 1 (readMaybe (last args))
+    else 1
 
 isTest :: [String] -> Bool
 isTest args = length args > 3 && args !! 3 == "test"
@@ -70,3 +74,31 @@ printHelp = do
     "For example: \"stack run --"
       ++ " 2024 1 2 test\" will run second"
       ++ " question of day1, 2024 year with test input."
+
+fontSize :: Int
+fontSize = 15
+
+animation :: [String] -> Int -> IO ()
+animation ss fps = do
+  setTargetFPS fps
+  let ls = lines (head ss)
+      width = length (head ls)
+      height = length ls
+      w = fromIntegral (width * fontSize) * 0.54 :: Double
+      h = fromIntegral (height * fontSize) * 1.13 :: Double
+  win <- initWindow (ceiling w) (ceiling h) "Animation"
+  font <- loadFont "assets/consola.ttf"
+  loop win font (cycle ss)
+ where
+  loop winr ft (f : fs) = do
+    shouldClose <- windowShouldClose
+    if not shouldClose
+      then do
+        beginDrawing
+        clearBackground white
+        drawTextEx ft f (V2 0 0) (fromIntegral fontSize) 0.0 black
+        endDrawing
+        loop winr ft fs
+      else do
+        closeWindow (Just winr)
+  loop _ _ [] = undefined
