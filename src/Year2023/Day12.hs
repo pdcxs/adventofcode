6f2559de1b7b3f1682bc0f8aa08c3ad428174c37
pdcxs <pdcxs@outlook.com>
2025-06-22 00:31:12 +0800

    Optimze 2023 Day 12
    Improve readability and performance.

--- src/Year2023/Day12.hs
+++ src/Year2023/Day12.hs
@@ -4,13 +4,14 @@ import Data.List (intersperse)
 import Data.List.Split (splitOn)
 import Data.MemoTrie (memo2)
 
-type Count = Int
+type Spring = Int
+type Lava = String
 
 processInput ::
   (String -> String) ->
   (String -> String) ->
   String ->
-  [(String, [Count])]
+  [(Lava, [Spring])]
 processInput preProcess1 preProcess2 =
   map processLine . lines
  where
@@ -20,70 +21,50 @@ processInput preProcess1 preProcess2 =
         s2' = preProcess2 (drop 1 s2)
      in (s1', map read $ splitOn "," s2')
 
-getAnswer :: (String, [Count]) -> Int
-getAnswer = uncurry putIn
+solve :: Lava -> [Spring] -> Int
+solve lava [] = if '#' `elem` lava then 0 else 1
+solve lava sps@(s : ss) =
+  sum $
+    map
+      ( \x ->
+          memo2 solve (drop (x + 1) lava) ss
+      )
+      valid
  where
-  putIn :: String -> [Count] -> Int
-  putIn cs [] =
-    if any (== '#') cs then 0 else 1
-  putIn [] [0] = 1
-  putIn [] _ = 0
-  putIn ('#' : _) (0 : _) = 0
-  putIn (_ : cs) (0 : cts) = memo2 putIn cs cts
-  putIn ('#' : cs) (c : cts)
-    | d < c - 1 = 0
-    | otherwise = case next of
-        [] -> memo2 putIn [] cts
-        ('#' : _) -> 0
-        (_ : cs') -> memo2 putIn cs' cts
-   where
-    ns = takeWhile (\x -> x == '#' || x == '?') cs
-    n = length ns
-    d = min n (c - 1)
-    next = drop d cs
-  putIn ('.' : cs) cts = memo2 putIn cs cts
-  putIn ('?' : []) [1] = 1
-  putIn ('?' : []) _ = 0
-  putIn ('?' : cs) (c : cts)
-    | n < c - 1 = case next of
-        ('#' : _) ->
-          sum $
-            map
-              (\x -> memo2 putIn next ((c - x) : cts))
-              [0 .. n + 1]
-        _ -> memo2 putIn next (c : cts)
-    | otherwise = case next of
-        [] -> memo2 putIn [] cts
-        ('?' : cs') ->
-          memo2 putIn cs' cts + memo2 putIn cs (c : cts)
-        ('#' : _) ->
-          sum $
-            map
-              (\x -> memo2 putIn next (x : cts))
-              [1 .. c]
-        _ ->
-          memo2 putIn (drop n cs) cts
-            + memo2 putIn cs (c : cts)
-   where
-    ns = takeWhile (== '?') cs
-    n = min (length ns) (c - 1)
-    next = drop n cs
-  putIn _ _ = undefined
+  n = length lava
+  starts = [0 .. n - sum sps - length ss]
+  checks =
+    map
+      ( \x ->
+          (x + s, take s (drop x lava))
+      )
+      starts
+  isValid (next, subLava) =
+    let start = next - s
+     in not $
+          next > n
+            || '#' `elem` take start lava
+            || '.' `elem` subLava
+            || startsWith '#' (drop next lava)
+  valid = map fst $ filter isValid checks
+
+startsWith :: (Eq a) => a -> [a] -> Bool
+startsWith _ [] = False
+startsWith x (y : _) = x == y
 
 solution1 :: String -> String
 solution1 =
   show
     . sum
-    . map getAnswer
+    . map (uncurry solve)
     . processInput id id
 
 solution2 :: String -> String
 solution2 =
   show
     . sum
-    . map getAnswer
+    . map (uncurry solve)
     . processInput pre1 pre2
  where
-  pre1 = timesWith "?"
-  pre2 = timesWith ","
-  timesWith c = concat . intersperse c . replicate 5
+  pre1 = concat . intersperse "?" . replicate 5
+  pre2 = concat . intersperse "," . replicate 5
