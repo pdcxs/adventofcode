2fda671718e8e775e9b12a366c82ae025a01290a
pdcxs <pdcxs@outlook.com>
2025-09-21 17:36:50 +0800

    2022 Day 05

--- src/Year2022/Solutions.hs
+++ src/Year2022/Solutions.hs
@@ -5,6 +5,7 @@ import qualified Year2022.Day01
 import qualified Year2022.Day02
 import qualified Year2022.Day03
 import qualified Year2022.Day04
+import qualified Year2022.Day05
 
 solutions :: M.Map (Int, Int, Int) (String -> IO ())
 solutions =
@@ -17,6 +18,8 @@ solutions =
   , ((2022, 3, 2), Year2022.Day03.solution2)
   , ((2022, 4, 1), Year2022.Day04.solution1)
   , ((2022, 4, 2), Year2022.Day04.solution2)
+  , ((2022, 5, 1), Year2022.Day05.solution1)
+  , ((2022, 5, 2), Year2022.Day05.solution2)
   ]
 
 animations :: M.Map (Int, Int, Int) (String -> [String])
