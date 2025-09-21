359105b4c8b9f53d10d9239cc399d1c1f8fad583
pdcxs <pdcxs@outlook.com>
2025-09-21 11:28:56 +0800

    2022 Day 03

---
+++ src/Year2022/Day03.hs
@@ -0,0 +1,45 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use bimap" #-}
+
+module Year2022.Day03 (solution1, solution2) where
+
+import Data.Char (isLower, ord)
+import Data.List (foldl1')
+import Data.List.Split (chunksOf)
+import qualified Data.Set as S
+
+getPriority :: Char -> Int
+getPriority c
+ | isLower c = ord c - ord 'a' + 1
+ | otherwise = ord c - ord 'A' + 27
+
+solution1 :: String -> IO ()
+solution1 =
+ print
+  . sum
+  . map
+   ( \l ->
+      sum
+       . S.map getPriority
+       . foldl1' S.intersection
+       . map S.fromList
+       . chunksOf (length l `div` 2)
+       $ l
+   )
+  . filter (not . null)
+  . lines
+
+solution2 :: String -> IO ()
+solution2 =
+ print
+  . sum
+  . map
+   ( sum
+      . S.map getPriority
+      . foldl1' S.intersection
+      . map S.fromList
+   )
+  . chunksOf 3
+  . filter (not . null)
+  . lines
--- src/Year2022/Solutions.hs
+++ src/Year2022/Solutions.hs
@@ -3,6 +3,7 @@ module Year2022.Solutions (solutions, animations) where
 import qualified Data.Map as M
 import qualified Year2022.Day01
 import qualified Year2022.Day02
+import qualified Year2022.Day03
 
 solutions :: M.Map (Int, Int, Int) (String -> IO ())
 solutions =
@@ -11,6 +12,8 @@ solutions =
   , ((2022, 1, 2), Year2022.Day01.solution2)
   , ((2022, 2, 1), Year2022.Day02.solution1)
   , ((2022, 2, 2), Year2022.Day02.solution2)
+  , ((2022, 3, 1), Year2022.Day03.solution1)
+  , ((2022, 3, 2), Year2022.Day03.solution2)
   ]
 
 animations :: M.Map (Int, Int, Int) (String -> [String])
