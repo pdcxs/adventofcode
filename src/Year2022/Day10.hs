cc74ea82b7e2e14ece19f999e526fda96d8e312d
pdcxs <pdcxs@outlook.com>
2025-11-06 18:23:51 +0800

    2022 Day 10

---
+++ src/Year2022/Day10.hs
@@ -0,0 +1,57 @@
+module Year2022.Day10 (solution1, solution2) where
+
+data Op = Noop | Inc Int
+
+processInput :: String -> [Op]
+processInput = map parse . lines
+ where
+  parse "noop" = Noop
+  parse s = Inc (read $ dropWhile (/= ' ') s)
+
+step :: Int -> Op -> [Int]
+step x Noop = [x]
+step x (Inc v) = [x + v, x]
+
+run :: [Op] -> [Int]
+run = reverse . go [1]
+ where
+  go vs [] = vs
+  go xs@(v : _) (o : os) = go (step v o ++ xs) os
+  go [] _ = undefined
+
+getResult :: [Int] -> [Int] -> Int
+getResult = go 1
+ where
+  go _ [] _ = 0
+  go t (d : ds) (v : vs)
+    | t == d = t * v + go (t + 1) ds vs
+    | otherwise = go (t + 1) (d : ds) vs
+  go _ _ _ = undefined
+
+solution1 :: String -> IO ()
+solution1 =
+  print
+    . getResult [20, 60 .. 220]
+    . run
+    . processInput
+
+generateLine :: [Int] -> (String, [Int])
+generateLine vs = (r, vs')
+ where
+  (xs, vs') = splitAt 40 vs
+  r = zipWith f xs [0 .. 39]
+  f x y = if abs (x - y) < 2 then '#' else '.'
+
+getScreen :: [Int] -> String
+getScreen vs =
+  let (r, vs') = generateLine vs
+   in if null vs'
+        then r
+        else r ++ "\n" ++ getScreen vs'
+
+solution2 :: String -> IO ()
+solution2 =
+  putStrLn
+    . getScreen
+    . run
+    . processInput
--- src/Year2022/Solutions.hs
+++ src/Year2022/Solutions.hs
@@ -10,29 +10,32 @@ import qualified Year2022.Day06
 import qualified Year2022.Day07
 import qualified Year2022.Day08
 import qualified Year2022.Day09
+import qualified Year2022.Day10
 
 solutions :: M.Map (Int, Int, Int) (String -> IO ())
 solutions =
- M.fromList
-  [ ((2022, 1, 1), Year2022.Day01.solution1)
-  , ((2022, 1, 2), Year2022.Day01.solution2)
-  , ((2022, 2, 1), Year2022.Day02.solution1)
-  , ((2022, 2, 2), Year2022.Day02.solution2)
-  , ((2022, 3, 1), Year2022.Day03.solution1)
-  , ((2022, 3, 2), Year2022.Day03.solution2)
-  , ((2022, 4, 1), Year2022.Day04.solution1)
-  , ((2022, 4, 2), Year2022.Day04.solution2)
-  , ((2022, 5, 1), Year2022.Day05.solution1)
-  , ((2022, 5, 2), Year2022.Day05.solution2)
-  , ((2022, 6, 1), Year2022.Day06.solution1)
-  , ((2022, 6, 2), Year2022.Day06.solution2)
-  , ((2022, 7, 1), Year2022.Day07.solution1)
-  , ((2022, 7, 2), Year2022.Day07.solution2)
-  , ((2022, 8, 1), Year2022.Day08.solution1)
-  , ((2022, 8, 2), Year2022.Day08.solution2)
-  , ((2022, 9, 1), Year2022.Day09.solution1)
-  , ((2022, 9, 2), Year2022.Day09.solution2)
-  ]
+  M.fromList
+    [ ((2022, 1, 1), Year2022.Day01.solution1)
+    , ((2022, 1, 2), Year2022.Day01.solution2)
+    , ((2022, 2, 1), Year2022.Day02.solution1)
+    , ((2022, 2, 2), Year2022.Day02.solution2)
+    , ((2022, 3, 1), Year2022.Day03.solution1)
+    , ((2022, 3, 2), Year2022.Day03.solution2)
+    , ((2022, 4, 1), Year2022.Day04.solution1)
+    , ((2022, 4, 2), Year2022.Day04.solution2)
+    , ((2022, 5, 1), Year2022.Day05.solution1)
+    , ((2022, 5, 2), Year2022.Day05.solution2)
+    , ((2022, 6, 1), Year2022.Day06.solution1)
+    , ((2022, 6, 2), Year2022.Day06.solution2)
+    , ((2022, 7, 1), Year2022.Day07.solution1)
+    , ((2022, 7, 2), Year2022.Day07.solution2)
+    , ((2022, 8, 1), Year2022.Day08.solution1)
+    , ((2022, 8, 2), Year2022.Day08.solution2)
+    , ((2022, 9, 1), Year2022.Day09.solution1)
+    , ((2022, 9, 2), Year2022.Day09.solution2)
+    , ((2022, 10, 1), Year2022.Day10.solution1)
+    , ((2022, 10, 2), Year2022.Day10.solution2)
+    ]
 
 animations :: M.Map (Int, Int, Int) (String -> [String])
 animations = M.empty
